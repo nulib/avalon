ea5564e3d23a465828f7bcdbd83a28a8d1e482bb
Michael Klein <mbklein@gmail.com>
2014-08-21 15:24:17 -0500

    Don't report ActiveFedora::ObjectNotFoundErrors to errbit

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,6 +1,7 @@
 Airbrake.configure do |config|
   config.api_key = 'a5e854c57ce269fd25d920f1f0afe87f'
-  config.host    = 'mbk-linux-dev.library.northwestern.edu'
+  config.host    = 'errbit.library.northwestern.edu'
   config.port    = 80
   config.secure  = config.port == 443
+  config.ignore << "ActiveFedora::ObjectNotFoundError"
 end
