287d91f3b2d253f26e4bb37148cb0653ebdd3b12
Adam Arling <adam.arling@northwestern.edu>
2017-05-03 16:51:08 -0500

    Docker environment setup

---
+++ db/migrate/20170503204749_create_annotations.active_annotations.rb
@@ -0,0 +1,6 @@
+# This migration comes from active_annotations (originally 20160422052041)
+class CreateAnnotations < ActiveRecord::Migration
+  def change
+    
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170118162908) do
+ActiveRecord::Schema.define(version: 20170503204749) do
 
   create_table "annotations", force: :cascade do |t|
     t.string  "uuid"
