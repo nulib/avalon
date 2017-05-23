6e74b644dffaf59833da25a029223e5a2fe3f357
Adam Arling <adam.arling@northwestern.edu>
2017-05-23 12:47:23 -0500

    Add About and Help stub pages; remove stubbed player and playlist selector from Browse

--- config/routes.rb
+++ config/routes.rb
@@ -164,6 +164,10 @@ Rails.application.routes.draw do
   end
   get '/jobs', to: redirect('/')
 
+  # NU Pages routing
+  get '/help', to: 'nu_pages#help'
+  get '/aboutnu', to: 'nu_pages#about'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
