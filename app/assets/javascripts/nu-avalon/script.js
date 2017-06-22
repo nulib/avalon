342449dc257032d89bc5a51766bdb8b9dccda21e
Adam Arling <adam.arling@northwestern.edu>
2017-06-22 11:26:55 -0500

    Add a loading indicator to Avalon media player for Chrome rendering delay

--- app/assets/javascripts/nu-avalon/script.js
+++ app/assets/javascripts/nu-avalon/script.js
@@ -13,10 +13,57 @@ function checkAlerts() {
   }
 }
 
+// Remove a node, after slight delay
 function removeNode(el) {
   setTimeout(function () {
     el.parentNode.removeChild(el);
   }, 500);
 }
 
+function removeLoader() {
+  console.log('removeLoader');
+  var loader = document.getElementsByClassName('loader');
+  if (loader.length > 0) {
+    loader[0].parentNode.removeChild(loader[0]);
+  }
+}
+
+function getAvalonPlayer() {
+  var el = document.getElementsByClassName('avalon-player');
+  if (el.length > 0) {
+    addLoader(el);
+  }
+}
+
+function addLoader(el) {
+  var loaderEl = document.createElement('div');
+  loaderEl.className = 'loader';
+  el[0].appendChild(loaderEl);
+  addPoller(15);
+}
+
+function addPoller(counter) {
+  if (counter > 0) {
+    var totalTimeEl = document.getElementsByClassName('mejs-duration');
+    if (totalTimeEl.length > 0) {
+      // Is it all Os or valid time?
+      if (totalTimeEl.innerText === '00:00') {
+        rePoll(counter);
+      } else {
+        removeLoader();
+      }
+    } else {
+      rePoll(counter);
+    }
+  }
+}
+
+function rePoll(counter) {
+  setTimeout(function() {
+    addPoller(counter - 1);
+  }, 1000);
+}
+
+
 checkAlerts();
+getAvalonPlayer();
