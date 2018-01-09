7e92c47084a8056ada4d60aee11acb3f0a42ca67
Carrick Rogers <carrickr@umich.edu>
2018-01-09 13:45:53 -0500

    add in activejob traffic control.  limit the batch entry processing to 2 per second and the active encode updates to 5 per second

--- app/jobs/active_encode_job.rb
+++ app/jobs/active_encode_job.rb
@@ -51,6 +51,8 @@ module ActiveEncodeJob
   class Update < ActiveJob::Base
     include ActiveEncodeJob::Core  #I'm not sure if the error callback is really makes sense here!
     queue_as :active_encode_update
+    throttle threshold: 5, period: 1.second
+
     def perform(master_file_id)
       Rails.logger.info "Updating encode progress for MasterFile: #{master_file_id}"
       mf = MasterFile.find(master_file_id)
