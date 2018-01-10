87b01e27ede13fc959db7e801cd61eee76980dd4
Carrick Rogers <carrickr@umich.edu>
2018-01-10 11:54:53 -0500

    slow down further on the initial ingest side

--- app/jobs/ingest_batch_entry_job.rb
+++ app/jobs/ingest_batch_entry_job.rb
@@ -17,7 +17,7 @@ class IngestBatchEntryJob < ActiveJob::Base
   # Throttle to this per second to stay within limit
   # for submitting to AWS Elastic Transcoder
   # https://docs.aws.amazon.com/elastictranscoder/latest/developerguide/limits.html
-  throttle threshold: 2, period: 1.second, drop: false
+  throttle threshold: 1, period: 10.second, drop: false
 
   # ActiveJob will serialize/deserialize the batch_entry automatically using GlobalIDs
   def perform(batch_entry)
