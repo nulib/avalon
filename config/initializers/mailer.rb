7843c554cfa58af2b4aec36028dfa92e2220286e
Michael Klein <mbklein@gmail.com>
2018-06-13 11:52:25 -0500

    Make sure aws-sdk-rails loads properly

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -1,4 +1,5 @@
 if File.exist?('/sys/hypervisor/uuid') && (File.read('/sys/hypervisor/uuid',3) == 'ec2')
+  require 'aws-sdk-rails'
   require 'aws/rails/mailer'
   ActionMailer::Base.delivery_method = :aws_sdk
 end
