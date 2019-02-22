3182f9d65fd43aa5ddc29c75b926893a3a9da573
Michael Klein <mbklein@gmail.com>
2019-02-22 14:58:38 -0600

    Embed retriable logic directly in Aws::ElasticTranscoder::Client.

---
+++ config/initializers/retry_elastic_transcoder.rb
@@ -0,0 +1,16 @@
+Aws::ElasticTranscoder::Client.prepend(
+  Module.new do
+    [:create_job, :read_job, :cancel_job].each do |method|
+      define_method(method) do |*method_args|
+        retry_proc = ->(exception, try, elapsed_time, next_interval) do
+          Rails.logger.warn("Exception calling `#{self.class.name}##{method}': #{exception.class}: `#{exception.message}' - #{try} tries in #{elapsed_time} seconds and #{next_interval} seconds until the next try.")
+        end
+      
+        Rails.logger.info("Calling `#{self.class.name}##{method}' with retriable enabled.")
+        Retriable.retriable(tries: 10, on_retry: retry_proc) do
+          super(*method_args)
+        end
+      end
+    end
+  end  
+)
