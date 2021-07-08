4f7e7b7c09ce1f793d73dede90f0de86cfe96240
Michael B. Klein <mbklein@gmail.com>
2021-07-07 23:06:52 -0500

    Initial pass to work with devstack & NU SSO auth
    Fix BlacklightHelperReloadFix scoping issue
    
    Upgrade to active_encode v0.8.0 for MediaConvert compatibility

--- config/initializers/avalon_lti.rb
+++ config/initializers/avalon_lti.rb
@@ -1,11 +1,13 @@
 # You also need to explicitly enable OAuth 1 support in the environment.rb or an initializer:
 AUTH_10_SUPPORT = true
 
+OAuth::Signature.available_methods.keys.each do |method|
+  OAuth::Signature.available_methods[method.upcase] = OAuth::Signature.available_methods[method]
+end
 module Avalon
   module Lti
     begin
-      Configuration =
-        YAML.load(ERB.new(File.read(File.expand_path('../../lti.yml', __FILE__))).result)
+      Configuration = YAML.load(File.read(File.expand_path('../../lti.yml',__FILE__)))
     rescue
       Configuration = {}
     end
