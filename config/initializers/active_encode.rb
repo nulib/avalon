f2e95361ca8748d3e8aa7b8b5faf47dc503ce572
Michael B. Klein <mbklein@gmail.com>
2021-07-21 00:31:51 -0500

    Add ActiveEncode MediaConvert configuration

--- config/initializers/active_encode.rb
+++ config/initializers/active_encode.rb
@@ -6,6 +6,10 @@ Rails.application.config.to_prepare do
     ActiveEncode::EngineAdapters::FfmpegAdapter.completeness_threshold = 95
   when :matterhorn
     Rubyhorn.init
+  when :mediaconvert
+    ActiveEncode::Base.engine_adapter.role = Settings.encoding.media_convert.role
+    ActiveEncode::Base.engine_adapter.output_bucket = Settings.encoding.derivative_bucket
+    ActiveEncode::Base.engine_adapter.setup!
   when :elastic_transcoder
     require 'aws-sdk-elastictranscoder'
     require 'avalon/elastic_transcoder_encode'
