a54065ce140bd6c876cf30f77b6d7fe9bcd123b7
Michael B. Klein <mbklein@gmail.com>
2021-07-29 13:19:59 -0500

    Add job to create adaptive playlists for pre-upgrade MasterFiles

--- terraform/queues.tf
+++ terraform/queues.tf
@@ -1,9 +1,9 @@
 locals {
   queue_names = [
-    "batch_ingest", "bulk_access_control", "create_encode", "default", "delete_course",
-    "extract_still", "ingest_finished_job", "ingest_status_job", "ingest", "master_file_management_delete",
-    "master_file_management_move", "reindex", "s3_split", "solr_backup", "update_dependent_permalinks",
-    "waveform"
+    "batch_ingest", "bulk_access_control", "create_adaptive_playlist", "create_encode", "default", 
+    "delete_course", "extract_still", "ingest_finished_job", "ingest_status_job", "ingest", 
+    "master_file_management_delete", "master_file_management_move", "reindex", "s3_split", 
+    "solr_backup", "update_dependent_permalinks", "waveform"
   ]
 }
 
