1718729dc98babfef7036e6e08092d9788754981
Michael B. Klein <mbklein@gmail.com>
2021-10-04 12:32:11 -0500

    [mediaconvert] Have MediaConvertEncode convert AIFFs to WAVs before transcoding

--- app/models/media_convert_encode.rb
+++ app/models/media_convert_encode.rb
@@ -1,3 +1,5 @@
+require 'aws-sdk-lambda'
+
 class MediaConvertEncode < WatchedEncode
   self.engine_adapter = :media_convert
   self.engine_adapter.queue = Settings&.encoding&.media_convert&.queue
@@ -5,6 +7,24 @@ class MediaConvertEncode < WatchedEncode
   self.engine_adapter.output_bucket = Settings&.encoding&.derivative_bucket
 
   before_create prepend: true do |encode|
+    if encode.input.url =~ %r{s3://(.+?)/(.+)\.aiff$}
+      wav_url = "s3://#{$1}/wav_temp/#{$2}.wav"
+      Honeybadger.notify(
+        "AIFF file found. Invoking aiff2wav.", 
+        class_name: "MediaConvertEncode", 
+        tags: "encoding", 
+        context: {
+          input_url: encode.input.url,
+          master_file_id: encode.options[:master_file_id]
+        }
+      )
+      Rails.logger.warn("#{encode.input.url} is AIFF. Creating intermediate WAV file #{wav_url}.")
+      lambda_client = Aws::Lambda::Client.new
+      payload = { source: encode.input.url, dest: wav_url }
+      lambda_client.invoke(function_name: Settings.encoding.aiff_lambda, payload: payload.to_json)
+      encode.input.url = wav_url
+    end
+    
     encode.options.merge!({
       masterfile_bucket: Settings.encoding.masterfile_bucket,
       output_prefix: "#{options[:master_file_id]}/",
@@ -17,6 +37,10 @@ class MediaConvertEncode < WatchedEncode
     map_outputs!(encode)
     record = ActiveEncode::EncodeRecord.find_by(global_id: encode.to_global_id.to_s)
     master_file = MasterFile.find(record.master_file_id)
+    if encode.input.url =~ %r{/wav_temp/}
+      Rails.logger.warn("Deleting intermediate WAV file #{encode.input.url}")
+      FileLocator::S3File.new(encode.input.url).object.delete
+    end
     master_file.update_progress_on_success!(encode)
   end
 
