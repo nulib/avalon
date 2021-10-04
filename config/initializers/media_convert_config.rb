afa894ad2f2d177c7ba2877c9a707ffdacc1fb9d
Michael B. Klein <mbklein@gmail.com>
2021-10-04 12:32:11 -0500

    [mediaconvert] Have MediaConvertEncode convert AIFFs to WAVs before transcoding

--- config/initializers/media_convert_config.rb
+++ config/initializers/media_convert_config.rb
@@ -1,13 +1,12 @@
 Settings.encoding.media_convert ||= Config::Options.new
 Settings.encoding.media_convert.configuration = {
-  mapping: {'1080' => 'high', '720' => 'medium', '540' => 'low'},
+  mapping: {'720' => 'high', '540' => 'low'},
   options: {
     'avalon' => {
       media_type: :video,
       outputs: [
-        {preset: "System-Avc_16x9_1080p_29_97fps_8500kbps", modifier: "-1080"},
-        {preset: "System-Avc_16x9_720p_29_97fps_5000kbps", modifier: "-720"},
-        {preset: "System-Avc_16x9_540p_29_97fps_3500kbps", modifier: "-540"}
+        {preset: "avr-video-medium", modifier: "-720"},
+        {preset: "avr-video-low", modifier: "-540"}
       ]
     },
     'fullaudio' => {
