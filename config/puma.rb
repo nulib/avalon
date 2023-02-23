63d47f2d7e0b030cdb2c735be5e5b25ac0fff98e
Michael B. Klein <mbklein@gmail.com>
2023-02-23 17:21:08 +0000

    [development] Point at correct SSL cert

--- config/puma.rb
+++ config/puma.rb
@@ -1,4 +1,4 @@
 bind 'tcp://0.0.0.0:3000'
 ssl_bind '0.0.0.0', '3001', 
-  cert: File.join(ENV['HOME'], '.dev_cert', 'dev.rdc.cert.pem'),
-  key: File.join(ENV['HOME'], '.dev_cert', 'dev.rdc.key.pem')
+  cert: ENV['SSL_CERT'],
+  key: ENV['SSL_KEY']
