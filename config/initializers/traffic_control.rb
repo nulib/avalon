a59b699698187d515887e13f3f426f140555d039
Michael B. Klein <mbklein@gmail.com>
2026-06-15 18:50:55 +0000

    Update to Avalon v8.0
    Add AVR custom guardfile & puma.rb from

--- config/initializers/traffic_control.rb
+++ config/initializers/traffic_control.rb
@@ -1,3 +1,5 @@
 # This initializer depends on the cache_store initializer being run first and the cache store being redis
-ActiveJob::TrafficControl.client = Redis.new(Rails.application.config.cache_store[1])
+config = Rails.application.config.cache_store[1]
+config.delete(:namespace)
+ActiveJob::TrafficControl.client = Redis.new(config)
 
